84c7471a3cb60006c68affd9be4497627a1075c6
Vik <vikoliveira.dev@gmail.com>
2025-10-21 15:54:29 -0300

    Adição dos containers para cada cor e a alteração do fade-out junto com o data-taget

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('.nav-link');
     const body = document.body;
-    
-    const classesDeCorFundo = ['bg-azul', 'bg-amarelo', 'bg-verde', 'bg-rosa'];
+    const containers = document.querySelectorAll('.container-imagens');
+
+    containers.forEach(container => {
+        container.style.display = 'none';
+    });
+    document.getElementById('home').style.display = 'block';
 
     body.classList.add('fade-in');
 
     navLinks.forEach(link => {
         link.addEventListener('click', function (event) {
-            event.preventDefault(); // 
+            event.preventDefault();
 
             body.classList.remove('fade-in');
             body.classList.add('fade-out');
 
-            navLinks.forEach(item => {
-                item.classList.remove('active');
-            });
+            navLinks.forEach(item => item.classList.remove('active'));
             this.classList.add('active');
-            
-            const novaCorFundo = this.getAttribute('data-cor-fundo');
+
+            const targetId = this.getAttribute('data-target');
 
             setTimeout(() => {
-                body.classList.remove(...classesDeCorFundo);
+                containers.forEach(container => {
+                    container.style.display = 'none';
+                });
 
-                if (novaCorFundo) {
-                    body.classList.add(novaCorFundo);
+                const targetContainer = document.getElementById(targetId);
+                if (targetContainer) {
+                    targetContainer.style.display = 'block';
                 }
 
                 body.classList.remove('fade-out');
                 body.classList.add('fade-in');
-            }, 500);
+            }, 300);
         });
     });
 });
